0b7a992add0407435a01cedbd8a25e7f215f042d
jcarrera94 <juankarrera@gmail.com>
2019-09-30 20:47:24 +0000

    added another partial to show the long url and short url through a separate path

--- express-server.js
+++ express-server.js
@@ -15,24 +15,16 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  res.render('urls-index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls-show', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
