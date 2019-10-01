d91832a99c94cbaefb9aa360cf4846d5472a055a
jcarrera94 <juankarrera@gmail.com>
2019-10-01 23:27:17 +0000

    added the edit option to each shortURL using the POST method

--- express-server.js
+++ express-server.js
@@ -47,6 +47,11 @@ app.post('/urls', (req, res) => {
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls/' + req.params.shortURL);
 })
 
 app.get('/u/:shortURL', (req, res) => {
