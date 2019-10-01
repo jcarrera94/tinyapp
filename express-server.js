8a319ba8f8e88df5f4f61d98e918613873c93068
jcarrera94 <juankarrera@gmail.com>
2019-10-01 16:48:24 +0000

    added the new form to creat a new tinyURL form the longURL the user input

--- express-server.js
+++ express-server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -18,10 +20,18 @@ app.get('/urls', (req, res) => {
   res.render('urls-index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls-new')
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls-show', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+})
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
